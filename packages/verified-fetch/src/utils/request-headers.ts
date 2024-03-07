698ee8ff9eb545703fa6c4bbc8b58834011f9f42
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-03-07 09:13:15 -0800

    test: add back header helpers

--- packages/verified-fetch/src/utils/request-headers.ts
+++ packages/verified-fetch/src/utils/request-headers.ts
@@ -16,3 +16,36 @@ export function getHeader (headers: HeadersInit | undefined, header: string): st
 
   return headers[key]
 }
+
+/**
+ * Given two ints from a Range header, and potential fileSize, returns:
+ * 1. number of bytes the response should contain.
+ * 2. the start index of the range. // inclusive
+ * 3. the end index of the range. // inclusive
+ */
+export function calculateByteRangeIndexes (start: number | undefined, end: number | undefined, fileSize?: number): { byteSize?: number, start?: number, end?: number } {
+  if (start != null && end != null) {
+    if (start > end) {
+      throw new Error('Invalid range')
+    }
+
+    return { byteSize: end - start + 1, start, end }
+  } else if (start == null && end != null) {
+    // suffix byte range requested
+    if (fileSize == null) {
+      return { end }
+    }
+    const result = { byteSize: end, start: fileSize - end, end: fileSize }
+    return result
+  } else if (start != null && end == null) {
+    if (fileSize == null) {
+      return { start }
+    }
+    const byteSize = fileSize - start + 1
+    const end = fileSize
+    return { byteSize, start, end }
+  }
+
+  // both start and end are undefined
+  return { byteSize: fileSize }
+}
---
+++ packages/verified-fetch/src/utils/response-headers.ts
@@ -0,0 +1,21 @@
+/**
+ * This function returns the value of the `Content-Range` header for a given range.
+ * If you know the total size of the body, you should pass it in the `options` object.
+ *
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Range
+ */
+export function getContentRangeHeader ({ byteStart, byteEnd, contentSize }: { byteStart: number | undefined, byteEnd: number | undefined, contentSize: number | undefined }): string {
+  const total = contentSize ?? '*' // if we don't know the total size, we should use *
+
+  if (byteStart == null && byteEnd == null) {
+    return `bytes */${total}`
+  }
+  if (byteStart != null && byteEnd == null) {
+    return `bytes ${byteStart}-*/${total}`
+  }
+  if (byteStart == null && byteEnd != null) {
+    return `bytes */${total}`
+  }
+
+  return `bytes ${byteStart}-${byteEnd}/${total}`
+}
