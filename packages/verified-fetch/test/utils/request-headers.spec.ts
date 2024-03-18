770c889cb9255f4130be43d4b91fc3ea20896409
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-03-18 14:58:15 -0700

    fix: request-headers

--- packages/verified-fetch/test/utils/request-headers.spec.ts
+++ packages/verified-fetch/test/utils/request-headers.spec.ts
@@ -34,7 +34,7 @@ describe('request-headers', () => {
       // Range: bytes=5-
       { start: 5, end: undefined, fileSize: 10, expected: { byteSize: 5, start: 5, end: 9 } },
       // Range: bytes=-5
-      { start: undefined, end: 5, fileSize: 10, expected: { byteSize: 5, start: 6, end: 9 } },
+      { start: undefined, end: 5, fileSize: 10, expected: { byteSize: 5, start: 5, end: 9 } },
       // Range: bytes=0-0
       { start: 0, end: 0, fileSize: 10, expected: { byteSize: 1, start: 0, end: 0 } },
       // Range: bytes=5- with unknown filesize
@@ -44,9 +44,14 @@ describe('request-headers', () => {
       // Range: bytes=0-0 with unknown filesize
       { start: 0, end: 0, fileSize: undefined, expected: { byteSize: 1, start: 0, end: 0 } },
       // Range: bytes=-9 & fileSize=11
-      { start: undefined, end: 9, fileSize: 11, expected: { byteSize: 9, start: 3, end: 10 } },
+      { start: undefined, end: 9, fileSize: 11, expected: { byteSize: 9, start: 2, end: 10 } },
       // Range: bytes=-11 & fileSize=11
-      { start: undefined, end: 11, fileSize: 11, expected: { byteSize: 11, start: 0, end: 10 } }
+      { start: undefined, end: 11, fileSize: 11, expected: { byteSize: 11, start: 0, end: 10 } },
+      // Range: bytes=-2 & fileSize=11
+      { start: undefined, end: 2, fileSize: 11, expected: { byteSize: 2, start: 9, end: 10 } },
+      // Range request with no range (added for coverage)
+      { start: undefined, end: undefined, fileSize: 10, expected: { byteSize: 10, start: 0, end: 9 } }
+
     ]
     testCases.forEach(({ start, end, fileSize, expected }) => {
       it(`should return expected result for bytes=${start ?? ''}-${end ?? ''} and fileSize=${fileSize}`, () => {
@@ -57,6 +62,7 @@ describe('request-headers', () => {
       expect(() => calculateByteRangeIndexes(5, 4, 10)).to.throw('Invalid range: Range-start index is greater than range-end index.')
       expect(() => calculateByteRangeIndexes(5, 11, 11)).to.throw('Invalid range: Range-end index is greater than or equal to the size of the file.')
       expect(() => calculateByteRangeIndexes(undefined, 12, 11)).to.throw('Invalid range: Range-end index is greater than the size of the file.')
+      expect(() => calculateByteRangeIndexes(-1, 5, 10)).to.throw('Invalid range: Range-start index cannot be negative.')
     })
   })
 })
