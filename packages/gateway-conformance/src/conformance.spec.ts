5d59de4cb011f003aaa51e15e47738ddf45dc190
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-04-30 19:21:34 -0700

    fix: split up conformance tests so its not one big long run

--- packages/gateway-conformance/src/conformance.spec.ts
+++ packages/gateway-conformance/src/conformance.spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-env mocha */
+import { readFile } from 'node:fs/promises'
 import { expect } from 'aegir/chai'
 import { execa } from 'execa'
 import { Agent, setGlobalDispatcher } from 'undici'
@@ -33,8 +34,6 @@ const testsToRun: string[] = [
 ]
 
 describe('@helia/verified-fetch - gateway conformance', function () {
-  this.timeout(60 * 1000)
-
   before(async () => {
     if (process.env.KUBO_GATEWAY == null) {
       throw new Error('KUBO_GATEWAY env var is required')
@@ -75,10 +74,8 @@ describe('@helia/verified-fetch - gateway conformance', function () {
     })
   })
 
-  it('gateway conformance', async () => {
-    const textDecoder = new TextDecoder()
-    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-    const subProcess = execa('docker', [
+  function getConformanceTestArgs (specToTest: string, additionalArgs: string[] = []): string[] {
+    return [
       'run',
       '--network',
       'host',
@@ -88,31 +85,68 @@ describe('@helia/verified-fetch - gateway conformance', function () {
       '/workspace',
       'ghcr.io/ipfs/gateway-conformance:v0.5.1',
       'test',
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       `--gateway-url=http://${process.env.CONFORMANCE_HOST!}:${process.env.PROXY_PORT!}`,
+      // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
       `--subdomain-url=http://${process.env.CONFORMANCE_HOST!}:${process.env.PROXY_PORT!}`,
       '--verbose',
-      '--json', 'gwc-report.json',
+      '--json', `gwc-report-${specToTest}.json`,
+      '-specs', specToTest,
       '--',
       '-timeout', '30m',
       // `-run=(${testsToRun.join('|')})`,
-      '-skip', `"(${testsToSkip.join('|')})"`
-    ])
+      // '-skip', `"(${testsToSkip.join('|')})"`
+      ...additionalArgs
+    ]
+  }
 
-    subProcess.stderr?.on('data', (data) => {
-      // convert Uint8Array to string
-      const text = textDecoder.decode(data)
-      // eslint-disable-next-line no-console
-      console.log('stderr text', text)
-      expect(text).not.to.contain('--- FAIL:')
-    })
-    subProcess.stdout?.on('data', (data) => {
-      const text = textDecoder.decode(data)
-      // eslint-disable-next-line no-console
-      console.log('stdout text', text)
-      expect(text).not.to.contain('--- FAIL:')
-      // expect(test).not.to.contain('no tests to run')
-    })
+  describe('conformance testing', () => {
+    const specs: Array<[string, number]> = [
+      ['trustless-block-gateway', 1],
+      ['trustless-car-gateway', 1],
+      ['trustless-car-gateway-optional', 1],
+      ['trustless-ipns-gateway', 1],
+      ['trustless-gateway', 1],
+      ['path-unixfs-gateway', 1],
+      ['path-tar-gateway', 1],
+      ['path-dag-gateway', 1],
+      ['path-raw-gateway', 1],
+      ['path-gateway', 1],
+      ['subdomain-ipfs-gateway', 1],
+      ['subdomain-ipns-gateway', 1],
+      ['subdomain-gateway', 1],
+      ['dnslink-gateway', 1],
+      ['redirects-file', 1]
+    ]
 
-    await expect(subProcess).to.eventually.be.fulfilled()
+    specs.forEach(([spec, maxFailingTestCount]) => {
+      it(`has minimal failing tests for ${spec} spec`, async function () {
+        this.timeout(5 * 60 * 1000)
+        // 5 minutes per spec
+        const subProcess = execa('docker', getConformanceTestArgs(spec))
+
+        let failureCount = 0
+        let successCount = 0
+
+        await (new Promise<void>((resolve, reject) => {
+          void subProcess.on('close', () => {
+            resolve()
+          })
+        }))
+
+        // parse the newline delimited JSON report at gwc-report.json and count the number of "PASS:" and "FAIL:" lines
+        const report = await readFile(`gwc-report-${spec}.json`, 'utf8')
+        const lines = report.split('\n')
+        for (const line of lines) {
+          if (line.includes('FAIL:')) {
+            failureCount++
+          } else if (line.includes('PASS:')) {
+            successCount++
+          }
+        }
+
+        expect(failureCount).to.be.lessThanOrEqual(maxFailingTestCount)
+      })
+    })
   })
 })
