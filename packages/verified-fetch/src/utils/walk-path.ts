3b41752e120025d67017eb34c40cab3c71346676
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-05-16 13:21:49 -0700

    fix: reduce dagPb and dagCbor handler complexity (#45)

--- packages/verified-fetch/src/utils/walk-path.ts
+++ packages/verified-fetch/src/utils/walk-path.ts
@@ -1,5 +1,8 @@
-import { CodeError } from '@libp2p/interface'
+import { CodeError, type Logger } from '@libp2p/interface'
+import { type Blockstore } from 'interface-blockstore'
 import { walkPath as exporterWalk, type ExporterOptions, type ReadableStorage, type ObjectNode, type UnixFSEntry } from 'ipfs-unixfs-exporter'
+import { type FetchHandlerFunctionArg } from '../types.js'
+import { badGatewayResponse, notFoundResponse } from './responses.js'
 import type { CID } from 'multiformats/cid'
 
 export interface PathWalkerOptions extends ExporterOptions {
@@ -36,4 +39,24 @@ export async function walkPath (blockstore: ReadableStorage, path: string, optio
 
 export function isObjectNode (node: UnixFSEntry): node is ObjectNode {
   return node.type === 'object'
+}
+
+/**
+ * Attempts to walk the path in the blockstore, returning ipfsRoots needed to resolve the path, and the terminal element.
+ * If the signal is aborted, the function will throw an AbortError
+ * If a terminal element is not found, a notFoundResponse is returned
+ * If another unknown error occurs, a badGatewayResponse is returned
+ */
+export async function handlePathWalking ({ cid, path, resource, options, blockstore, log }: Omit<FetchHandlerFunctionArg, 'session'> & { blockstore: Blockstore, log: Logger }): Promise<PathWalkerResponse | Response> {
+  try {
+    return await walkPath(blockstore, `${cid.toString()}/${path}`, options)
+  } catch (err: any) {
+    options?.signal?.throwIfAborted()
+    if (['ERR_NO_PROP', 'ERR_NO_TERMINAL_ELEMENT', 'ERR_NOT_FOUND'].includes(err.code)) {
+      return notFoundResponse(resource)
+    }
+
+    log.error('error walking path %s', path, err)
+    return badGatewayResponse(resource, 'Error walking path')
+  }
 }
