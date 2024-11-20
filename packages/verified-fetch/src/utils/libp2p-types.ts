0cc392b0d1fd4e2125accf58b0af168d5a75cef1
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2024-11-20 12:48:42 -0600

    fix: use matching index signature for Libp2pServices

--- packages/verified-fetch/src/utils/libp2p-types.ts
+++ packages/verified-fetch/src/utils/libp2p-types.ts
@@ -4,12 +4,11 @@ import type { DefaultLibp2pServices } from 'helia'
 import type { Libp2pOptions } from 'libp2p'
 
 type BaseServiceMap = Record<string, unknown>
-type DelegatedRoutingServices = Record<`delegatedRouting${number}`, unknown | ((components?: unknown) => DelegatedRoutingV1HttpApiClient)>
+type DelegatedRoutingServices = Record<`delegatedRouting${number}`, ((components?: unknown) => DelegatedRoutingV1HttpApiClient)>
 
-// eslint-disable-next-line @typescript-eslint/consistent-indexed-object-style
-export interface Libp2pServices extends BaseServiceMap, DelegatedRoutingServices {
-  [key: string]: any
-}
+export type Libp2pServices<T extends ServiceMap = ServiceMap> = {
+  [Property in keyof T]: (components: any & T) => T[Property]
+} & BaseServiceMap & DelegatedRoutingServices
 
 interface Libp2pInitOverride<T extends ServiceMap = Libp2pServices & DefaultLibp2pServices> extends Omit<Libp2pOptions<T>, 'services'> {
   services: Libp2pServices
