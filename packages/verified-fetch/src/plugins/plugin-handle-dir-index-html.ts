3754648c818febe2fbc7a19868d627fd9bdcd24c
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-04-22 10:53:02 -0500

    fix: dir-index-html has correct content type

--- packages/verified-fetch/src/plugins/plugin-handle-dir-index-html.ts
+++ packages/verified-fetch/src/plugins/plugin-handle-dir-index-html.ts
@@ -1,5 +1,6 @@
 import { code as dagPbCode } from '@ipld/dag-pb'
 import { dirIndexHtml } from '../utils/dir-index-html.js'
+import { getIpfsRoots } from '../utils/response-headers.js'
 import { okRangeResponse } from '../utils/responses.js'
 import { BasePlugin } from './plugin-base.js'
 import type { PluginContext, VerifiedFetchPluginFactory } from './types.js'
@@ -25,18 +26,21 @@ export class DirIndexHtmlPlugin extends BasePlugin {
   async handle (context: PluginContext & Required<Pick<PluginContext, 'byteRangeContext' | 'pathDetails' | 'directoryEntries'>>): Promise<Response> {
     const { resource, pathDetails, directoryEntries } = context
 
-    const terminalElement = pathDetails.terminalElement
+    const { terminalElement, ipfsRoots } = pathDetails
 
     const gatewayURL = resource
     const htmlResponse = dirIndexHtml(terminalElement, directoryEntries, { gatewayURL, log: this.log })
 
     context.byteRangeContext.setBody(htmlResponse)
 
-    const response = okRangeResponse(resource, context.byteRangeContext.getBody(), { byteRangeContext: context.byteRangeContext, log: this.log })
-
-    response.headers.set('Content-Type', 'text/html')
-    // see https://github.com/ipfs/gateway-conformance/pull/219
-    response.headers.set('Cache-Control', 'public, max-age=604800, stale-while-revalidate=2678400')
+    const response = okRangeResponse(resource, context.byteRangeContext.getBody(), { byteRangeContext: context.byteRangeContext, log: this.log }, {
+      headers: {
+        'Content-Type': 'text/html',
+        // see https://github.com/ipfs/gateway-conformance/pull/219
+        'Cache-Control': 'public, max-age=604800, stale-while-revalidate=2678400',
+        'X-Ipfs-Roots': getIpfsRoots(ipfsRoots)
+      }
+    })
 
     return response
   }
