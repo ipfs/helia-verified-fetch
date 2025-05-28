9205c3af06a051034884332911b299aba25aa94d
Russell Dempsey <1173416+SgtPooki@users.noreply.github.com>
2025-05-28 15:56:56 -0400

    fix: implement dag-scope using traversal and exporter car options

--- packages/verified-fetch/src/plugins/plugin-handle-car.ts
+++ packages/verified-fetch/src/plugins/plugin-handle-car.ts
@@ -1,8 +1,9 @@
-import { car } from '@helia/car'
+import { BlockExporter, car, CIDPath, SubgraphExporter, UnixFSExporter, type ExportCarOptions } from '@helia/car'
 import toBrowserReadableStream from 'it-to-browser-readablestream'
 import { okRangeResponse } from '../utils/responses.js'
 import { BasePlugin } from './plugin-base.js'
 import type { PluginContext } from './types.js'
+import { code as dagPbCode } from '@ipld/dag-pb'
 
 function getFilename ({ cid, ipfsPath, query }: Pick<PluginContext, 'query' | 'cid' | 'ipfsPath'>): string {
   if (query.filename != null) {
@@ -40,18 +41,48 @@ export class CarPlugin extends BasePlugin {
     if (context.byteRangeContext == null) {
       return false
     }
+    if (context.pathDetails == null) {
+      return false
+    }
+
     return context.accept?.startsWith('application/vnd.ipld.car') === true || context.query.format === 'car' // application/vnd.ipld.car
   }
 
-  async handle (context: PluginContext): Promise<Response> {
+  async handle (context: PluginContext & Required<Pick<PluginContext, 'byteRangeContext'>>): Promise<Response> {
     const { options, pathDetails, cid } = context
+    if (pathDetails == null) {
+      throw new Error('attempted to handle request for car with no path details')
+    }
     const { getBlockstore, helia } = this.pluginOptions
     context.reqFormat = 'car'
     context.query.download = true
     context.query.filename = getFilename(context)
     const blockstore = getBlockstore(cid, context.resource, options?.session ?? true, options)
-    const c = car({ blockstore, getCodec: helia.getCodec })
-    const stream = toBrowserReadableStream(c.stream(pathDetails?.terminalElement.cid ?? cid, options))
+
+    const c = car({
+      blockstore,
+      getCodec: helia.getCodec,
+      logger: helia.logger
+    })
+    const carExportOptions: ExportCarOptions = {
+      ...options,
+      traversal: new CIDPath(pathDetails.ipfsRoots)
+    }
+    const dagScope = getDagScope(context)
+    if (dagScope === 'block') {
+      carExportOptions.exporter = new BlockExporter()
+    } else if (dagScope === 'all') {
+      carExportOptions.exporter = new SubgraphExporter()
+    } else if (dagScope === 'entity') {
+      // if its unixFS, we need to enumerate a directory, or get all blocks for the entity, otherwise, use blockExporter
+      if (pathDetails.terminalElement.cid.code === dagPbCode) {
+        carExportOptions.exporter = new UnixFSExporter()
+      } else {
+        carExportOptions.exporter = new BlockExporter()
+      }
+    }
+    const stream = toBrowserReadableStream(c.stream(pathDetails?.terminalElement.cid ?? cid, carExportOptions))
+    context.byteRangeContext.setBody(stream)
 
     const response = okRangeResponse(context.resource, context.byteRangeContext.getBody(), { byteRangeContext: context.byteRangeContext, log: this.log })
     response.headers.set('content-type', 'application/vnd.ipld.car; version=1')
