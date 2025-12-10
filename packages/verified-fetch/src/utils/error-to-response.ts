5255d7269bb742d4346d483dd3336dc8c4e25910
achingbrain <alex@achingbrain.net>
2025-12-10 17:40:32 +0200

    chore: 406 on bad dag-pb

--- packages/verified-fetch/src/utils/error-to-response.ts
+++ packages/verified-fetch/src/utils/error-to-response.ts
@@ -1,5 +1,6 @@
-import { badGatewayResponse, gatewayTimeoutResponse, internalServerErrorResponse, notFoundResponse } from './responses.js'
-import type { Resource } from '../index.js'
+import { badGatewayResponse, gatewayTimeoutResponse, internalServerErrorResponse, notAcceptableResponse, notFoundResponse } from './responses.js'
+import { MEDIA_TYPE_RAW, type Resource } from '../index.js'
+import { CONTENT_TYPE_OCTET_STREAM, CONTENT_TYPE_RAW } from './content-types.ts'
 
 export function errorToResponse (resource: Resource | string, err: any): Response {
   // if a signal abort caused the error, throw the error
@@ -12,15 +13,25 @@ export function errorToResponse (resource: Resource | string, err: any): Respons
     return badGatewayResponse(resource.toString(), err)
   }
 
+  // data was not parseable, user may be able to request raw block
+  if (['NotUnixFSError'].includes(err.name)) {
+    return notAcceptableResponse(resource.toString(), [
+      CONTENT_TYPE_RAW,
+      CONTENT_TYPE_OCTET_STREAM
+    ])
+  }
+
   // an upstream server didn't respond in time but inside the signal timeout
   if (err.code === 'ETIMEOUT' || err.name === 'TimeoutError') {
     return gatewayTimeoutResponse(resource.toString(), err)
   }
 
+  // path was not under DAG root
   if (['ERR_NO_PROP', 'ERR_NO_TERMINAL_ELEMENT', 'ERR_NOT_FOUND'].includes(err.code)) {
     return notFoundResponse(resource.toString())
   }
 
+  // path was not under DAG root
   if (['DoesNotExistError'].includes(err.name)) {
     return notFoundResponse(resource.toString())
   }
