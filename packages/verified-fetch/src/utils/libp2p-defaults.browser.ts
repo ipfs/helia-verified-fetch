873cba2cacffc4a634d5c2d603159cd9692b66fa
achingbrain <alex@achingbrain.net>
2026-08-15 10:46:01 +0100

    deps: update @helia/libp2p to 2.x.x
    Update dep to latest version

--- packages/verified-fetch/src/utils/libp2p-defaults.browser.ts
+++ packages/verified-fetch/src/utils/libp2p-defaults.browser.ts
@@ -5,7 +5,7 @@ import type { ServiceFactoryMap } from './libp2p-types.ts'
 import type { DefaultLibp2pServices } from '@helia/libp2p'
 import type { Libp2pOptions } from 'libp2p'
 
-type ServiceMap = Pick<DefaultLibp2pServices, 'dcutr' | 'identify' | 'keychain' | 'ping'>
+type ServiceMap = Pick<DefaultLibp2pServices, 'dcutr' | 'identify' | 'identifyPush' | 'keychain' | 'ping'>
 
 export function getLibp2pConfig (): Libp2pOptions & Required<Pick<Libp2pOptions, 'services'>> {
   const libp2pDefaultOptions = libp2pDefaults()
@@ -18,6 +18,7 @@ export function getLibp2pConfig (): Libp2pOptions & Required<Pick<Libp2pOptions,
   const services: ServiceFactoryMap<ServiceMap> = {
     dcutr: libp2pDefaultOptions.services.dcutr,
     identify: libp2pDefaultOptions.services.identify,
+    identifyPush: libp2pDefaultOptions.services.identifyPush,
     keychain: libp2pDefaultOptions.services.keychain,
     ping: libp2pDefaultOptions.services.ping
   }
